c81c4c3eb9dce8b4f5c10a9b1868edf298898658
Teerapon Chatchatree <ztreeraponc@pttep.com>
2024-10-04 11:03:32 +0700

    Update login.service.ts

--- shop-naja/src/app/service/login.service.ts
+++ shop-naja/src/app/service/login.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { SignIn } from '../model/sign-in';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +14,6 @@ export class LoginService {
       'Content-Type': 'application/json'
     })
 
-    let params = new HttpParams({})
-    params.set('email', 'teerapon.ch@vulcan.bluebik.com')
-    params.set('password', '010743P')
     const body = {email: 'teerapon.ch@vulcan.bluebik.com', password: '010743P'};
     return this.http.post<SignIn>(`https://shop.eco-deals.com/services/api/v1/sign-in`, body, { headers: headers  , observe: 'body'})
     }
